345f31f9dd44a807efe9f3b3ae31d986bce23e53
Mandyiee <chiomzibae101@gmail.com>
2022-12-20 10:01:10 +0100

    fixing the login issue19

--- cents/settings.py
+++ cents/settings.py
@@ -136,9 +136,9 @@ USE_TZ = True
 
 SITE_ID = 1
 
-ACCOUNT_ADAPTER = 'user_core.adapter.MyAccountAdapter'
 
-ACCOUNT_SIGNUP_REDIRECT = '/user/'
+
+ACCOUNT_SIGNUP_REDIRECT = '/'
 
 LOGIN_REDIRECT_URL = '/user/'
 
